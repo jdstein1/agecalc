01560a962f43deb011dd65106a62d84f9a099675
Stein, Jeff <jstein0@searshc.com>
2015-12-16 15:54:09 -0600

    Add polyfill for Object.keys() to utils.  Make basic func for traversing date JSON.

--- app/scripts/compare.js
+++ app/scripts/compare.js
@@ -1,5 +1,5 @@
 // jshint devel:true
-$(function() {
+// $(function() {
   // Handler for .ready() called.
 
 /* --------------------------------- */
@@ -8,9 +8,8 @@ $(function() {
 
   var sampleText = document.createTextNode('SAMPLE TEXT');
 
-  var $intervalBox = $('.interval-box');
   var intervalBox = document.getElementsByClassName('interval-box');
-  console.log('intervalBox: ', intervalBox);
+  // console.log('intervalBox: ', intervalBox);
 
   var newInterval = document.createElement('div');
   newInterval.classList.add('well','well-sm','interval');
@@ -18,31 +17,66 @@ $(function() {
   var newIntervalRow = document.createElement('div');
   newIntervalRow.classList.add('row','interval-dates');
   newIntervalRow.appendChild(sampleText);
-  document.body.appendChild(newIntervalRow);
-  // document.intervalBox.appendChild(newIntervalRow);
-  $intervalBox.append($(newIntervalRow));
+  // document.body.appendChild(newIntervalRow);
+  // $intervalBox.append($(newIntervalRow));
+  // intervalBox[0].appendChild(newIntervalRow);
 
   var loadDates = function (obj) {
-    console.log(obj);
+    console.group('obj: ',obj);
+    var objKeysI = Object.keys(obj);
 
-    for (var i = 0; i < obj.length; i++) {
-      console.log(obj[i]);
-    };
+    if (objKeysI.length > 0) {
+      console.log('objKeysI: ',objKeysI);
+      console.log('objKeysI.length: ',objKeysI.length);
+
+      for (var i = 0; i < objKeysI.length; i++) {
+        console.group('obj['+objKeysI[i]+']: ',obj[objKeysI[i]]);
+        var objKeysJ = Object.keys(obj[objKeysI[i]]);
+
+        if (objKeysJ.length > 0) {
+          console.log('objKeysJ: ',objKeysJ);
+          console.log('objKeysJ.length: ',objKeysJ.length);
+
+          for (var j = 0; j < objKeysJ.length; j++) {
+            if (obj[objKeysI[i]][objKeysJ[j]]) {
+              console.group('obj['+objKeysI[i]+']['+objKeysJ[j]+']: ',obj[objKeysI[i]][objKeysJ[j]]);
+              var objKeysK = Object.keys(obj[objKeysI[i]][objKeysJ[j]]);
+
+              if (objKeysK.length > 0) {
+                console.log('objKeysK: ',objKeysK);
+                console.log('objKeysK.length: ',objKeysK.length);
+                for (var k = 0; k < objKeysK.length; k++) {
+                  if (obj[objKeysI[i]][objKeysJ[j]][objKeysK[k]]) {
+                    console.group('obj['+objKeysI[i]+']['+objKeysJ[j]+']['+objKeysK[k]+']: ',obj[objKeysI[i]][objKeysJ[j]][objKeysK[k]]);
+                  }
+                };
+              }
+    
+              console.groupEnd();
+
+            }
+  
+          };
+
+        }
+
+        console.groupEnd();
+
+      };
+
+    }
+
+    console.groupEnd();
 
   };
 
   $('#robin').bind('click', function () {
-    loadDates(dates.robin)
+    loadDates(dates.robin);
   });
-  // $('#robin').on('click', function () {
-  //   loadDates(dates.robin);
-  // });
-  $('#susan').bind('click', function () {
-    loadDates(dates.susan)
-  });
-  // $('#susan').on('click', function () {
-  //   loadDates(dates.susan)
-  // });
 
-});
+  $('#susan').bind('click', function () {
+    loadDates(dates.susan);
+  });
+
+// });
 
--- app/scripts/utils.js
+++ app/scripts/utils.js
@@ -2,6 +2,15 @@
 // $(function() {
   // Handler for .ready() called.
 
+// http://tokenposts.blogspot.com.au/2012/04/javascript-objectkeys-browser.html
+if (!Object.keys) Object.keys = function(o) {
+  if (o !== Object(o))
+    throw new TypeError('Object.keys called on a non-object');
+  var k=[],p;
+  for (p in o) if (Object.prototype.hasOwnProperty.call(o,p)) k.push(p);
+  return k;
+}
+
   var checkLength = function (thing,expected) {
     console.group('START checkLength (thing.length is less than '+expected+')');
     if (typeof thing === 'object') {
